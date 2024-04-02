8d63724adfa5200b729d1c260c4f7abf5c00c4a8
Nekena <claudiarktz@gmail.com>
2024-04-02 21:42:03 +0200

    Creation de l'interface Consommation

--- src/iut/bad/Humain.java
+++ src/iut/bad/Humain.java
@@ -1,6 +1,6 @@
 package iut.bad;
 
-public class Humain{
+public class Humain implements Consommation {
    	protected String nom;
 	protected String prenom;
 	protected int age;
@@ -44,10 +44,12 @@ public class Humain{
         	return "Nom: " + nom + ", Prénom: " + prenom + ", Âge: " + age;
     	}
 
+	@Override
 	public void manger() {
 
     	}
 
+	@Override
     	public void boire() {
         
     	}
