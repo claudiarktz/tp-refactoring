abf7ec1ea123822e8fb59bc60de97f4d89157d59
Nekena <claudiarktz@gmail.com>
2024-04-02 21:47:49 +0200

    Creation de main() et instanciation

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -10,4 +10,12 @@ public class Femme extends Humain{
         //	return "Femme: " + super.toString();
     	//}
 
+	public static void main(String[] args) {
+        	Homme homme = new Homme("Alexander", "Stewart", 28);
+        	Femme femme = new Femme("Anne", "Marie", 27);
+
+        	// Déclarer que l'un est ami de l'autre
+        	femme.ami(homme);
+    	}
+
 }
